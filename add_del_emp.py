ce299ab853a1b907f403298bae042c17572cf5ce
rima <rimarabinovich92@gmail.com>
2020-10-07 19:21:29 +0300

    after documentation

--- add_del_emp.py
+++ add_del_emp.py
@@ -1,7 +1,7 @@
 import csv
 
 class Employee:
-
+    # each employee must have id,name,phone,age
     def __init__(self, employee_id, name, phone, age):
         self.employee_id = employee_id
         self.name = name
@@ -9,31 +9,38 @@ class Employee:
         self.age = age
 
 class ID9Digits(Exception):
+    # error to be raised when user's id is not valid
     def __str__(self):
         return('ID must be 9 digits')
 
 class NameNotValid(Exception):
+    # error to be raised when user's name is not valid
     def __str__(self):
         return('Name must be composed from alphabetic characters')
 
 class Phone10Digits(Exception):
+    # error to be raised when user's phone is not valid
     def __str__(self):
         return('Phone number must be 10 digits')
 
 class AgeNotValid(Exception):
+    # error to be raised when user's age is not valid
     def __str__(self):
         return('Age must be composed from 2 digits')
 
 class NoSuchEmployee(Exception):
+    # error to be raised when employee doesn't exist in the employee file
     def __str__(self):
         return('There is no such Name/ID')
 
 def empty_employee_file():
+    #creates an empty csv file with the column names
     with open('employee_file.csv', mode='w') as employee_file:
         employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         employee_writer.writerow(['Employee ID', 'Employee Name', "Employee's Phone", "Employee's Age"])
 
 def add_employee_manually():
+    # a function to add employee to employee file
     print('\n~~Add employee manually~~')
     while True:
         try:
@@ -71,12 +78,15 @@ def add_employee_manually():
             print("ERROR: Age must be composed from 2 digits")
         else:
             break
+    #create an employee object
     employee = Employee(emp_id,emp_name,emp_phone,emp_age)
+    #add employee to file
     with open('employee_file.csv', mode='a+') as employee_file:
         employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         employee_writer.writerow([employee.employee_id, employee.name, employee.phone, employee.age])
 
 def add_employee_from_file():
+    # adds employees from csv file to the employee list
         print('\n~~Add employee from file~~')
         try:
             file = input("Insert employees to add CSV file: ")
@@ -84,58 +94,72 @@ def add_employee_from_file():
                 csv_reader = csv.reader(file, delimiter = ',')
                 line_count = 0
                 for row in csv_reader:
+                    # skip empty rows
                     if line_count == 0 or row == []:
                         line_count += 1
                         continue
                     else:
+                        # create employee object
                         new_employee = Employee(row[0],row[1],row[2],row[3])
+                        # add employee to employee file
                         with open('employee_file.csv', mode='a+') as employee_file:
                             employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
                             employee_writer.writerow([new_employee.employee_id, new_employee.name, new_employee.phone, new_employee.age])
                         line_count += 1
         except FileNotFoundError:
             print("ERROR: There's no such file : {}".format(file))
+        # when a file doesn't act like csv file
         except IndexError:
             print("ERROR: Something is wrong, Please check your file. \neach employee mast have: ID, Name, Phone and Age with a delimiter of ','.")
 
 def delete_employee_manually():
+    # a function to delete employee from employee file by id/name
     print("\n~~Delete employee manually~~")
     while True:
         try:
             user_input = input('Please enter employees name/id to delete: ')
+            # a list that will include all the employees but the one to be deleted
             lines = list()
             count = 0
             with open('employee_file.csv', 'r') as readFile:
                 reader = csv.reader(readFile)
                 for row in reader:
+                    #slip empty rows
                     if row == []:
                         continue
                     else:
                         lines.append(row)
+                        # check if employee's id or name are equal to user input
                         for field in row:
                             if field == user_input:
                                 count += 1
                                 lines.remove(row)
+            # if nothing matches user's input raise error
             if count == 0:
                 raise NoSuchEmployee()
         except NoSuchEmployee:
             print("ERROR: There is no such Name/ID")
         else:
+            # write the employee file again without the deleted employee
             with open('employee_file.csv', 'w') as writeFile:
                 writer = csv.writer(writeFile)
                 writer.writerows(lines)
                 break
         
 def delete_employee_from_file():
+    # a function to delete employees from file with a csv file
     print("\n~~Delete employee from file~~")
     while True:
         try:
             del_emp = input("Insert employees to delete CSV File: ")
+            # a list for the employee file to be deleted
             emp_to_delete = list()
+            # a list for existing employees
             emp_list = list()
             with open(del_emp, 'r') as readFile:
                 reader = csv.reader(readFile)
                 for row in reader:
+                    # skip empty row
                     if row == []:
                         continue
                     else:
@@ -143,17 +167,20 @@ def delete_employee_from_file():
             with open('employee_file.csv', 'r') as readFile:
                 reader = csv.reader(readFile)
                 for row in reader:
+                    # skip empty row
                     if row == []:
                         continue
                     else:
                         emp_list.append(row)
                         for i in emp_to_delete:
                             for field in row:
+                                # checks if employees id to be deleted matches the existing employees - if so, delete
                                 if field == i[0]:
                                     emp_list.remove(row)
         except FileNotFoundError:
             print("ERROR: There's no such file")
         else:
+            # rewrite employee file
             with open('employee_file.csv', 'w') as writeFile:
                     writer = csv.writer(writeFile)
                     writer.writerows(emp_list)
--- attendance_emp.py
+++ attendance_emp.py
@@ -19,74 +19,98 @@ def search_id_in_file():
             user_id = input("Please enter employee ID: ")
             emp_list = list()
             user_name = ''
+            # creates list of employees read from file
             with open('employee_file.csv', 'r') as readFile:
                 reader = csv.reader(readFile)
                 for row in reader:
+                    # skip empty row
                     if row == []:
                         continue
                     else:
                         emp_list.append(row)
             for i in emp_list:
+                # checks if the id matches
                 if i[0] == user_id:
                     user_name = i[1]
+            # raise error if id wasn't found
             if user_name == '':
                 raise NoSuchID()
         except NoSuchID:
             print('ERROR: There is no such ID in the employee file.')
         else:
+            # get current date and time
             user_date = (date.today()).strftime("%d/%m/%Y")
             user_time = (datetime.now()).strftime("%H:%M:%S")
             return user_id, user_name, user_date, user_time
 
 def generate_report_of_employee():
+    # creates a report for a spesific employee by id
     print("\n~~Generate attendance report~~")
     while True:
         try:
             user_id = input("Enter employee's ID: ")
             with open('attendance_file.txt','r') as readFile:
                 att_list = readFile.readlines()
+            # a list to store all employees attendance
             emp_att = []
             for i in att_list:
                 if user_id in i:
                     emp_att.append(i)
+            # raise error if id wasn't found
             if len(emp_att) == 0:
                 raise NoSuchID()
         except NoSuchID:
             print('ERROR: There is no such ID in the employee file.')
         else:
+            # write report file from the created list of a spesific employee
             with open('attendance_report.txt','w') as writeFile:
                 for i in emp_att:
                     writeFile.write(i)
                 break
 
 def generate_report_current_month():
+    # get current month
     month = str((datetime.today()).month)
+    #read attendance file to a list
     with open('attendance_file.txt','r') as readFile:
         att_list = readFile.readlines()
+        # write attendence file only for the current month
     with open('Monthly_attendance.txt','w') as writeFile:
         for i in att_list:
+            # spliting each row by space and then split again with '/' in order to get only the month
             i = i.split(' ')
             i[2] = i[2].split('/')
             if i[2][1] == month:
+                # join each row back to str in order to write it to file
                 i[2] = ' '.join(i[2])
                 i = ' '.join(i)
                 writeFile.write(i)
 
 def generate_report_late_att():
+    # get current date and time in order to get only the time
     late = datetime.now()
+    # changing the time to 9:30
     late = late.replace(hour = 9, minute = 30, second = 0, microsecond = 0)
+    # getting only the time
     late = late.time()
+    # create list from file
     with open('attendance_file.txt','r') as readFile:
         att_list = readFile.readlines()
+    # write file of all late attendance
     with open('late_attendance.txt','w') as writeFile:
         for i in att_list:
+            # spliting each row in order to get date and time
             i = i.split(' ')
             i[2] = i[2].split('/')
             i[3] = i[3].strip()
             i[3] = i[3].split(':')
+            # getting the time from attendance file in a format we can use to compare
             hour = datetime(int(i[2][2]),int(i[2][1]),int(i[2][0]),int(i[3][0]),int(i[3][1]),int(i[3][2]))
+            # getting only the time
             hour = hour.time()
+            # get the rows that are later than 9:30
             if hour > late:
+                # join the row back to str in order to write to file
                 i[3] = ' '.join(i[3])
                 i[2] = ' '.join(i[2])
                 i = ' '.join(i)
